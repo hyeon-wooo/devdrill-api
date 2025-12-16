02e793ef1f3dc31780c9e312b28ba2a16430ce58
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-12-16 13:52:01 +0900

    chore: db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DBConfigService } from './config/DBConfigService';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DBConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
