73b4bfd43cb1713ef93a63460d478723d3b91752
hyeonwoo <hyeonwoo.v24@gmail.com>
2026-01-15 15:32:47 +0900

    fix: exam subject entity import

--- src/exam/exam.module.ts
+++ src/exam/exam.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExamEntity } from './infra/exam.entity';
 import { QuestionModule } from 'src/question/question.module';
 import { ExamSubjectService } from './app/subject.service';
+import { ExamSubjectEntity } from './infra/subject.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExamEntity]), QuestionModule],
+  imports: [
+    TypeOrmModule.forFeature([ExamEntity, ExamSubjectEntity]),
+    QuestionModule,
+  ],
   controllers: [ExamController, ExamController],
   providers: [ExamService, ExamSubjectService],
   exports: [ExamService],
