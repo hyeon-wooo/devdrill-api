87a0a371306ed7ff782d12735741a1b2ac43c41e
hyeonwoo <hyeonwoo.v24@gmail.com>
2026-03-24 23:49:40 +0900

    fix: redis retry

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -12,11 +12,6 @@ import { REDIS_CLIENT_TOKEN } from './redis.constant';
       provide: REDIS_CLIENT_TOKEN,
       useFactory: (configService: ConfigService) => {
         const logger = new Logger('RedisClient');
-        logger.log(
-          `redis: ${configService.get<string>('REDIS_HOST')}:${configService.get<number>(
-            'REDIS_PORT',
-          )}, ${configService.get<string>('REDIS_USER')}, ${configService.get<string>('REDIS_PW')}`,
-        );
         const redis = new Redis({
           host: configService.get<string>('REDIS_HOST'),
           port: configService.get<number>('REDIS_PORT'),
@@ -24,12 +19,32 @@ import { REDIS_CLIENT_TOKEN } from './redis.constant';
           password: configService.get<string>('REDIS_PW'),
           // 첫 명령 시까지 연결 지연 (부팅 방지 핵심)
           lazyConnect: true,
-          // 자동 재연결 전략: FIXME: 재연결 전략 꼼꼼히 다시 짜야함
-          retryStrategy: (times) => Math.min(times * 50, 2000),
+          retryStrategy: (times) => {
+            const logger = new Logger('RedisClient');
+
+            if (times > 5) {
+              logger.error(
+                `Redis reconnect failed after ${times} attempts. Stopping retry.`,
+              );
+              return null; // 재시도 중단
+            }
+
+            // 지수 백오프
+            // 100ms * n
+            const delay = times * 100;
+
+            logger.warn(
+              `Redis connection lost. Retry attempt #${times} in ${delay}ms...`,
+            );
+            return delay;
+          },
         });
 
         redis.on('error', (err) =>
-          logger.error('Redis Client Error: ' + err.message),
+          logger.error('Redis Client Error: ' + err.message + err.stack),
+        );
+        redis.on('ready', () =>
+          logger.log('Redis Client Ready (Auth Success)'),
         );
         redis.on('connect', () => logger.log('Redis Client Connected'));
         redis.on('close', () => logger.log('Redis Client Disconnected'));
